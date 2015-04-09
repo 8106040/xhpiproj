61fe0735bd9cf9207146fb39e29fc7d2f6cf587f
xuhaoruins@hotmail.com <xuhaoruins@hotmail.com>
2015-04-09 14:15:38 +0800

    new

--- azure_blob_upload.py
+++ azure_blob_upload.py
@@ -2,4 +2,8 @@ from azure.storage import BlobService
 
 bs=BlobService(account_name='xhpi',account_key='LdfaFhlJpL8xJE3iFhgExDU8VRmA6s5M7b2s0Ztd2CrxX+6MVsFTHilHOJ3TuUCffluyaOgP7jYj8Na5J8g3+A==')
 
-bs.put_block_blob_from_path('xhpicloud','pic.jpg','/home/pi/1.jpg')
+bs.put_block_blob_from_path('xhpicloud','pic.jpg','/home/pi/blobfile/pic.jpg')
+
+bs.put_block_blob_from_path('xhpicloud','picpu.txt','/home/pi/blobfile/picpu.txt')
+
+bs.put_block_blob_from_path('xhpicloud','envtemp.txt','/home/pi/blobfile/envtemp.txt')
--- pi_cpu_temp.py
+++ pi_cpu_temp.py
@@ -4,7 +4,7 @@ tempfile = open ('/sys/class/thermal/thermal_zone0/temp' )
 
 pitemp = float(tempfile.read()) / 1000
 
-tempupload = file('/home/pi/blobfile/tempfile.txt', 'w')
+tempupload = file('/home/pi/blobfile/picpu.txt', 'w')
 
 print >> tempupload, pitemp
  
